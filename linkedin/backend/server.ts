735ea83ab27d0247dbd492af77ced716e45d84dd
sazzad90 <bsse1207@iit.du.ac.bd>
2023-08-10 02:46:28 +0600

    server.ts changed

--- linkedin/backend/server.ts
+++ linkedin/backend/server.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+//hello world
 const app = express();
 app.use(cors());
 app.use(express.json());
